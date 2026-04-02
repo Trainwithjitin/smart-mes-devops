6251bb79f90399147d5de2c3da764aea55915b1b
Jitin Ballal <jitinballal03@gmail.com>
2026-04-02 21:26:03 +0530

    Added CI/CD pipeline

--- MES.py
+++ MES.py
@@ -516,4 +516,4 @@ def assembly_operations():
 # ---------------- RUN APP ----------------
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
